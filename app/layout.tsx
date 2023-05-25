507129a8d7bf51ad9eecb4b503effb463c19f001
ÐAgentRa <bubonik.san1993@gmail.com>
2023-05-25 19:34:03 +0200

    Add basic layout and the Home page

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import '@styles/global.css'
+
+export const metadata = {
+  title: "Promptopia", description: "Discover & Share AI Prompts"
+}
+
+const RootLayout = ({ children }) => {
+  return (<html lang="en">
+    <body>
+      <div>
+        <div className="main">
+          <div className="gradient"></div>
+        </div>
+
+        <main className="app">
+          {children}
+        </main>
+      </div>
+    </body>
+    </html>)
+}
+
+export default RootLayout
